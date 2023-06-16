a8b1a19e51740486adef41be3b6ade45315e34ff
czolojd3000 <masllo.maslo@gmail.com>
2023-06-16 22:44:00 +0200

    feat: added fonts, small fixes to expo and git

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Helvetica: require('./assets/fonts/Helvetica.ttf'),
+  })
+
+  if(!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.j to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
